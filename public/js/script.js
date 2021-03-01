9a6e35e2af99cf52b91b695a983ff0670dc47d2b
asgarov <fasgarov2002@gmail.com>
2021-03-01 15:20:41 +0400

    update

--- public/js/script.js
+++ public/js/script.js
@@ -200,3 +200,15 @@ $(document).ready(function () {
     }
   });
 });
+
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:0,
+  nav:false,
+  items: 1,
+  autoplay:true,
+  autoplayTimeout:4500,
+  autoplayHoverPause:false,
+  dots: false
+})
